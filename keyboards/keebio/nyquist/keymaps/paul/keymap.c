59bd97dab08acb716181c8f61dad310b93d2fc40
Paul Elder <paul.elder@ideasonboard.com>
2019-03-05 20:52:50 -0500

    paul: add altgr

--- keyboards/keebio/nyquist/keymaps/paul/keymap.c
+++ keyboards/keebio/nyquist/keymaps/paul/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |Shift(|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift)|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Mouse | Comp | Alt  | GUI  |Lower |Space |Raise | Ctrl | Left | Down |  Up  |Right |
+ * |Mouse |AltGr | Alt  | GUI  |Lower |Space |Raise | Ctrl | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  *
  * Have to figure out how to do the compose key
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
   KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, \
-  MOUSE,   XXXXXXX, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  RAISE,   KC_RCTL, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  MOUSE,   KC_ALGR, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  RAISE,   KC_RCTL, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
 /* Lower
