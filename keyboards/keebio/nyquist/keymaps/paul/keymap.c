36826a6a66d8f68c1bf6629042d44856d918f58d
Paul Elder <paul.elder@ideasonboard.com>
2021-03-02 12:52:21 +0900

    paul: swap alt and ctrl in ff14 layout, and restore bottom row

--- keyboards/keebio/nyquist/keymaps/paul/keymap.c
+++ keyboards/keebio/nyquist/keymaps/paul/keymap.c
@@ -101,19 +101,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |   \  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |Enter |
+ * | Alt  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |Enter |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |Shift(|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  /   |Shift)|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Alt  | CAS  |  CA  |  AS  |  CS  |Space |Lower |Raise | Left | Down |  Up  |Right |
+ * | Ctrl |AltGr |  Alt | GUI  |Lower |Space |Raise |SpCtrl| Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_FFXIV] = LAYOUT( \
   KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
-  KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
+  KC_LALT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
   KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, \
-  KC_LALT, C(S(KC_LALT)), C(KC_LALT), S(KC_LALT), C(KC_LSHIFT), KC_SPC,  LOWER,   RAISE,   KC_LEFT, KC_DOWN, KC_UP, KC_RGHT \
+  KC_LCTL, KC_ALGR, KC_RALT, KC_LGUI, LOWER,   KC_SPC,  RAISE,   RCTL_T(KC_SPC), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT \
 ),
 
 /* Lower
